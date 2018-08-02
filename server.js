aef14d216d0f5dae0efff38f04750471e336b29d
Samkough Sammy (GPC2TNG) <GPC2TNG@ups.com>
2018-08-02 13:16:05 -0400

    Setup express.

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-// test
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send("Hello world!")
+    // res.render('index');
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
