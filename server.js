b081899f60a8c61624909aa388719ae0e00bf8ad
Samkough <samkoughs1@admsu.montclair.edu>
2018-10-26 12:48:08 -0400

    Initialized Twitter stuffs

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+var twit = requre('twit');
+
 const app = express();
 
 app.get('/', function (req, res) {
     res.send("Hello world!")
     // res.render('index');
 })
+
+app.get('/twitter', (req, res) => {
+    res.send("twitter");
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
